3d239110cccbede93d9aad94bedcee16c28b960a
dropp <droppmail@gmail.com>
2015-11-16 19:43:46 +0300

    Carcass

--- competition/views.py
+++ competition/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from annoying.decorators import render_to
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from competition.models import TextIdea, DesignIdea
+
+class TextIdeaList(ListView):
+    model = TextIdea
+    paginate_by = 20
+    template_name = 'textlist.html'
+
+
+class DesignIdeaList(ListView):
+    model = DesignIdea
+    paginate_by = 20
+    template_name = 'imglist.html'
+
+
+@render_to('index.html')
+def index(request):
+
+    pass
 
 # Create your views here.
